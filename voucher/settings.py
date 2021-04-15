c0f4dd63d876a2ab11b60c39c08f93fc54402028
SDlearner-maker <55377781+SDlearner-maker@users.noreply.github.com>
2021-04-15 12:30:09 +0800

    Update settings.py
    to add a package for coupon.

--- voucher/settings.py
+++ voucher/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_simple_coupons',
     'crispy_forms',
     'users.apps.UsersConfig',
     'homepage.apps.HomepageConfig',
